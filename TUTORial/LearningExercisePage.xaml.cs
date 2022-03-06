a43a904d701b5630d6006a6c0525339b2a42a06e
Will Harris <willharris0407@gmail.com>
2022-03-05 23:11:51 -0500

    Initial Commit

---
+++ TUTORial/LearningExercisePage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace TUTORial
+{
+    public partial class LearningExercisePage : ContentPage
+    {
+        private readonly string[] quotes =
+        {
+            "your", "text", "goes", "here"
+        };
+        private int quoteIndex = 0;
+
+        public LearningExercisePage()
+        {
+            InitializeComponent();
+
+            quoteLabel.Text = quotes[0];
+        }
+
+        void CycleText(object sender, EventArgs e)
+        {
+            ++quoteIndex;
+            if (quoteIndex >= quotes.Length) quoteIndex = 0;
+
+            quoteLabel.Text = quotes[quoteIndex];
+        }
+    }
+}
---
+++ TUTORial/LearningPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace TUTORial
+{
+    public partial class LearningPage : ContentPage
+    {
+        public LearningPage()
+        {
+            InitializeComponent();
+            slider.Value = 0.5;
+        }
+
+        void Cringe(object sender, EventArgs e)
+        {
+            DisplayAlert("Bruh", "Cringe", "ok.");
+        }
+
+        void ResetSlider(object sender, EventArgs e)
+        {
+            slider.Value = 0.5;
+        }
+    }
+}
